9718fee7b8280994b935de13e7d2057fb9b3bfb2
Kirti Vardhan <kirtivardhan8@gmail.com>
2024-03-15 20:50:02 +0530

    Project Created

---
+++ src/main/java/com/service/account/YobAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YobAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YobAccountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/account/YobAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YobAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
